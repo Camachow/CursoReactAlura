10adec63a7a9487c08214c5d512c1534e2654e01
Rafael Camacho <rafaelcamachofitti15@gmail.com>
2022-12-25 16:28:06 -0300

    Escondendo times sem card

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -4,12 +4,14 @@ import './Time.css'
 const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria}
     return (
-        <section className='time' style={css}>
+        /*Poderia trocar o ? por && e tirar a segunda opção de retorno*/
+        props.colaboradores.length > 0 ? <section className='time' style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='cards'>
                 {props.colaboradores.map( colaborador => <Card nome={colaborador.nome} cargo={colaborador.cargo} imagem = {colaborador.imagem}/>)}
             </div>
         </section>
+        : ''
     )
 }
 

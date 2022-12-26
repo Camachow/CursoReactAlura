1a374a1091fe36d81cce31a5ac5905f4786e6b65
Rafael Camacho <rafaelcamachofitti15@gmail.com>
2022-12-26 17:09:15 -0300

    feat/add background color no card

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,9 +1,9 @@
 import './Card.css'
 
-const Card = ({nome, imagem, cargo}) => {
+const Card = ({nome, imagem, cargo, cor}) => {
     return(
         <div className='card'>
-            <div className='cabecalho'>
+            <div className='cabecalho' style={{backgroundColor: cor}}>
                 <img src={imagem} alt={nome}/>
             </div>
             <div className='rodape'>
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -8,7 +8,7 @@ const Time = (props) => {
         props.colaboradores.length > 0 ? <section className='time' style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='cards'>
-                {props.colaboradores.map( colaborador => <Card key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem = {colaborador.imagem}/>)}
+                {props.colaboradores.map( colaborador => <Card key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem = {colaborador.imagem} cor = {props.corPrimaria}/>)}
             </div>
         </section>
         : ''

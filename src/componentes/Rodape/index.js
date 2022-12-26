b02f7ac5ca814a9f9c1b411b776a54f4ebb406cf
Rafael Camacho <rafaelcamachofitti15@gmail.com>
2022-12-26 00:13:20 -0300

    feat/add footer

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,38 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='footer'>
+            <section>
+                <ul>
+                    <li>
+                        <a href="facebook.com" target="_blank">
+                            <img src="/imagens/fb.png" alt="Facebook icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="twitter.com" target="_blank">
+                            <img src="/imagens/tw.png" alt="Twitter icon"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="instagram.com" target="_blank">
+                            <img src="/imagens/ig.png" alt="Instagram icon"/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src="imagens/logo.png" alt="Logo do Organo"/>
+            </section>
+            <section>
+            <p>
+                Desenvolvido por Alura.
+            </p>
+        </section>
+        </footer>
+
+    )
+}
+
+export default Rodape
